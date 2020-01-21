bb8446eeb7a34d4ac38d705ff0a1b3cb59d0f66b
adriana <adrianapenate98@gmail.com>
2020-01-21 16:16:41 +0000

    Red test - not_rearrange_when_there_are_no_letter

---
+++ src/test/java/ScrambliesShould.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScrambliesShould {
+    @Test
+    public void not_rearrange_when_there_are_no_letter() {
+        assertThat(Scramblies.scramble("", "hello")).isFalse();
+    }
+
+    private static class Scramblies {
+        public static boolean scramble(String pieces, String goal) {
+            return true;
+        }
+    }
+}
