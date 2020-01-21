bfe7661a1e19cfb7f9c4da79fd40ea5ecbdb2312
adriana <adrianapenate98@gmail.com>
2020-01-21 17:11:26 +0000

    Refactor - Class moved to its own file

--- src/test/java/ScrambliesShould.java
+++ src/test/java/ScrambliesShould.java
@@ -17,38 +17,4 @@ public class ScrambliesShould {
     public void not_rearrange_when_there_are_not_needed_letters() {
         assertThat(Scramblies.scramble("holua", "hello")).isFalse();
     }
-
-    private static class Scramblies {
-        public static boolean scramble(String pieces, String goal) {
-            if (areNotEnoughPieces(pieces, goal)) return false;
-            int pieceSelected = 0;
-            while (!goalHasLetters(goal) && areUncheckedPieces(pieces, pieceSelected)){
-                if (containsSelectedLetter(goal, pieces, pieceSelected)){
-                    goal = removeLetterFromGoal(pieces, goal, pieceSelected);
-                }
-                pieceSelected++;
-            }
-            return goalHasLetters(goal);
-        }
-
-        private static String removeLetterFromGoal(String pieces, String goal, int pieceSelected) {
-            return goal.replaceFirst(pieces.charAt(pieceSelected)+"", "");
-        }
-
-        private static boolean containsSelectedLetter(String goal, String pieces, int pieceSelected) {
-            return goal.contains(pieces.charAt(pieceSelected) + "");
-        }
-
-        private static boolean areUncheckedPieces(String pieces, int pieceSelected) {
-            return pieceSelected < pieces.length();
-        }
-
-        private static boolean goalHasLetters(String goal) {
-            return goal.isEmpty();
-        }
-
-        private static boolean areNotEnoughPieces(String pieces, String goal) {
-            return pieces.length() < goal.length();
-        }
-    }
 }
