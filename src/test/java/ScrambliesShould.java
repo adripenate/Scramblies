b5156cf98b507b43657044fc1230a526ad9aefca
adriana <adrianapenate98@gmail.com>
2020-01-21 16:24:07 +0000

    Refactor - Specific test deleted

--- src/test/java/ScrambliesShould.java
+++ src/test/java/ScrambliesShould.java
@@ -4,11 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScrambliesShould {
     @Test
-    public void not_rearrange_when_there_are_no_letter() {
-        assertThat(Scramblies.scramble("", "hello")).isFalse();
-    }
-
-    @Test
     public void rearrange_when_there_are_letters() {
         assertThat(Scramblies.scramble("holle", "hello")).isTrue();
     }
